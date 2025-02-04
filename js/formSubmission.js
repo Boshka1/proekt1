891d7938c2f0fc7bee6846633f3bdd465d414e3a
Boshka1 <edukub112@gmail.com>
2025-02-04 15:14:07 +0300

    Update formSubmission.js

--- js/formSubmission.js
+++ js/formSubmission.js
@@ -12,7 +12,7 @@ export function initFormSubmission() {
   
 		const formData = new FormData(feedbackForm);
   
-		fetch('https://formcarry.com/s/U1_4O8sQsWN', {
+		fetch('https://formcarry.com/s/GNUZA7_lclc', {
 			method: 'POST',
 			headers: {
 				Accept: 'application/json',
